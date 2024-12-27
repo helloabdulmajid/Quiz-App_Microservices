ee25f23e7f85c0c3648185e7301c50cef9c3f30b
Abdul Majid <helloabdulmajid@gmail.com>
2024-12-28 01:32:19 +0530

    Question-Service Complete & Run Multiple instances

---
+++ question-service/src/main/java/com/abdulmajid/question_service/model/Response.java
@@ -0,0 +1,35 @@
+package com.abdulmajid.question_service.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Data
+@RequiredArgsConstructor
+public class Response {
+
+    private Integer id;
+    private String response;
+
+    public Response(Integer id, String response) {
+        this.id = id;
+        this.response = response;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
